b1dd7ec1ded8c348c2cbea599d6e1d337c030a89
Danilo <damilhome@gmail.com>
2024-11-13 11:50:39 -0300

    verifica se duas cartas são iguais

--- script.js
+++ script.js
@@ -3,16 +3,30 @@ const resetBtn = document.getElementById('reset-btn');
 const gameDifficulty = document.getElementById('game-difficulty');
 const gameCards = document.getElementById('game-cards');
 const easy = 24;
-let turnedCards = 0;
+let turnedCards = [];
+
+const checkMatch = () => {
+    if(turnedCards[0].textContent === turnedCards[1].textContent) {
+        turnedCards[0].classList.add('match');
+        turnedCards[1].classList.add('match');
+    } else {
+        turnedCards[0].classList.remove('open');
+        turnedCards[1].classList.remove('open');
+    }
+
+    turnedCards = [];
+}
 
 const turnCard = (e) => {
     e.target.classList.add('open');
-    turnedCards++
+    turnedCards.push(e.target);
 
-    if(turnedCards == 2) {
-        
+    if(turnedCards.length === 2) {
+        console.log('check match')
+        setTimeout(checkMatch, 500)
     }
 }
+
 
 window.onload = () => {
     if(gameDifficulty.value === 'easy') {
